83e9e074a50e8ee14af33f880f4e0b5c48252187
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-01-23 20:11:09 +0800

    added function 2

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed." ;
 }
+
+function myFunction2() {
+    const element = document.getElementsByTagName("p");
+    document.getElementById("demo").innerHTML = "The text in first paragraph (index 0) is: " + element[0].innerHTM;
+}
