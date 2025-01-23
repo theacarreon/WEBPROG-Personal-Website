7906dfc986fdea50e7fa20910401ef573b03ba28
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-01-23 20:14:06 +0800

    updated function 2

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -4,5 +4,5 @@ function myFunction() {
 
 function myFunction2() {
     const element = document.getElementsByTagName("p");
-    document.getElementById("demo").innerHTML = "The text in first paragraph (index 0) is: " + element[0].innerHTM;
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
 }
